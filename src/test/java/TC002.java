14b3764e8bbb31391d8143a34bdd99f0fd4c6870
sermis97 <sermis97@gmail.com>
2021-09-14 12:12:49 +0300

    part1 for tc002

--- src/test/java/TC002.java
+++ src/test/java/TC002.java
@@ -1,15 +1,4 @@
 public class TC002 {
-<<<<<<< HEAD
 
     //work 1 completed
-
-    //work 2 completed
-
-
-    //added part 3 as requested
-
-    //testcase finished
-
-=======
->>>>>>> 5d4189d (STARTED for tc002)
 }
